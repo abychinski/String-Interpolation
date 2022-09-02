3e3140a04655a42f987156cf32d5baf23dc887c3
Andrew Bychinski <aebychinski@live.com>
2022-09-01 22:43:17 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace String_Interpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = DateTime.Now;
+           
+            var pi = Math.PI;
+
+            Console.WriteLine(date);
+
+            Console.WriteLine($"{date,40:MMMM d, yyyy}");
+            Console.WriteLine($"{date:yyyy.MM.dd}");
+            Console.WriteLine($"Year:{date:yyyy}, Month:{date:MM}, Day:{date:dd}");
+            Console.WriteLine($"{date,10:dddd}");
+            Console.WriteLine($"{date,10:t}{date,10:dddd}");
+            Console.WriteLine($"h:{date:hh}, m:{date:mm}, s:{date:ss}");
+            Console.WriteLine($"{date:yyyy}.{date:MM}.{date:dd}.{date:hh}.{date:mm}.{date:ss}");
+
+            Console.WriteLine($"{pi:C}");
+            Console.WriteLine($"{pi,10:n3}");
+
+        }
+    }
+}
+
+        
